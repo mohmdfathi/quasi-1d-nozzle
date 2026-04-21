493304773946b57213790853e47cc8e01b34754f
Mohamad Fathi <M.Fathi@tudelft.nl>
2026-04-21 12:37:16 +0200

    feat: add initial condition for primitive variables (p, T, rho, u)

--- fortran/nozzle_1D.f90
+++ fortran/nozzle_1D.f90
@@ -8,6 +8,11 @@ program nozzle_1D
     real, allocatable :: Ai(:)    ! nozzle cross-sectional area at each grid point
     real, allocatable :: dAdxi(:) ! derivative of area dA/dx at each grid point
 
+    real, allocatable :: T(:)     ! static temperature
+    real, allocatable :: p(:)     ! static pressure
+    real, allocatable :: rho(:)   ! density
+    real, allocatable :: u(:)     ! axial velocity
+
 
     ! allocate arrays and generate 1D grid
     allocate(xi(0:Imax), Ai(0:Imax), dAdxi(0:Imax))
@@ -23,8 +28,23 @@ program nozzle_1D
     write(*,*) "Nozzle area and gradients computed."
 
     
+    ! allocate primitive variables
+    allocate(T(0:Imax), p(0:Imax), rho(0:Imax), u(0:Imax))
+
+    ! initialize primitive variables
+    ! based on isentropic relations (M = 0.5)
+    associate( fac => 1.0 + 0.5*(k-1.0)*0.5**2 )
+    T   = T0 / fac             ! static temperature from isentropic relations
+    P   = P0 / fac**(k/(k-1.)) ! static pressure from isentropic relations
+    rho = P / ( R * T )        ! density from ideal gas law
+    u   = 0.5 * sqrt( kR * T ) ! velocity magnitude (from Mach definition)
+    end associate
+    write(*,*) "Primitive variables initialized."
+ 
+
+    
     ! cleanup
-    deallocate(xi, Ai, dAdxi)
+    deallocate(xi, Ai, dAdxi, T, p, rho, u)
     write(*,*) "Done. Memory deallocated."
 
 end program nozzle_1D
