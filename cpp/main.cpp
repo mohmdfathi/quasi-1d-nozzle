c01108eb389c2b8a7fd1e721a1275f748da87130
Mohamad Fathi <M.Fathi@tudelft.nl>
2026-05-19 11:29:41 +0200

    init: cpp project structure

---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "=====================================\n";
+    std::cout << " Quasi-1D Nozzle Solver (C++)\n";
+    std::cout << "=====================================\n";
+
+    return 0;
+}
